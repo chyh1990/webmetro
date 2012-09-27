974478065b98344c617b8b6de76e299d400f4531
Chen Yuheng <chyh1990@gmail.com>
2012-09-27 19:49:32 +0800

    add metroBlock

--- webmetro.js
+++ webmetro.js
@@ -1,6 +1,6 @@
 function getBlocksWidth(){
 	var w = 0;
-	$('.block').each(function(){
+	$('.metroblock').each(function(){
 		w += $(this).outerWidth(true);
 	});
 	w += 40;
@@ -9,9 +9,10 @@ function getBlocksWidth(){
 
 function setBlocksHeight(){
 	var h = $('#content').innerHeight() - 120;
-	$('.block').height(h);
+	$('.metroblock').height(h);
 }
 
+//main scroll bar
 (function( $ ) {
 	$.fn.jMainScroll = function(viewport, content) {
 		var obj = {
@@ -40,22 +41,29 @@ function setBlocksHeight(){
 				var p = pos * f;
 				this._bar.css('left', p+'px');
 				this._content.css('left', -pos+'px');
+			},
+			reinitialize: function(resetPos){
+				var vs = this._view.width()
+				, cs = this._content.width();
+				this._ratio = vs / cs;
+				if(this._ratio >= 1.0){
+					return obj;
+				}
+				var myw = this._track.width();
+				var barw = vs / cs * myw;
+				bar.css('width', barw + 'px');
+				if(resetPos)
+					this._pos = 0;
 			}
 		};
-		var vs = viewport.width()
-		, cs = content.width();
-		obj._ratio = vs / cs;
-		if(obj._ratio >= 1.0){
-			return obj;
-		}
+
 		this.css('margin', '0').css('padding', '0');
 		var bar = $('<div class="main-scroll-bar" style="margin:0;padding:0;"></div>').appendTo(this);
 		bar.css('height', '100%').css('position','absolute').css('top', '0');
 		bar.hide();
-		var myw = this.width();
-		var barw = vs / cs * myw;
-		bar.css('width', barw + 'px');
 		obj._bar = bar;
+		obj._track = this;
+		// using the event helper
 		content.mousewheel(function(event, delta, deltaX, deltaY) {
 			if(obj._bar == null)
 				return;
@@ -76,24 +84,174 @@ function setBlocksHeight(){
 		    obj.setDelta(v * obj._wheelSpeed);
 			//console.log(delta, deltaX, deltaY);
 			event.preventDefault();
-		});		
+		});
+		obj.reinitialize(true);
 		return obj;
 	};
 })( jQuery );
 
+//metro block
+(function( $ ) {
+	var methods = {
+     init : function( options ) {
+       return this.each(function(){
+         var $this = $(this),
+             data = $this.data('metroblk')
+			;
+
+         // If the plugin hasn't been initialized yet
+         if ( ! data ) {
+			var obj = new MetroBlock($this);
+           $(this).data('metroblk', {
+               target : $this,
+			   obj    : obj
+           });
+         }
+       });
+     },
+    	destroy : function( ) {
+		},
+		setContent : function(jsonStr) {
+			return this.each(function(){
+				var $this = $(this),
+				data = $this.data('metroblk');
+				if(data){
+					data.obj.setContent(jsonStr);
+				}
+			});
+		},
+		reinitialize: function() {
+			return this.each(function(){
+				var $this = $(this),
+				data = $this.data('metroblk');
+				if(data){
+					data.obj.reinitialize();
+				}
+			});			
+		}
+	};
+	$.fn.jMetroBlock = function(method){
+	    if ( methods[method] ) {
+	      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+	    } else if ( typeof method === 'object' || ! method ) {
+	      return methods.init.apply( this, arguments );
+	    } else {
+	      $.error( 'Method ' +  method + ' does not exist on jQuery.jMetroBlock' );
+	    }
+		return this;
+	};
+	//the object
+    function MetroBlock( root )
+    {
+        this.oWrapper    = root;
+		this.title = 'Unknown';
+		this.options = {
+			tileSize: 130,
+			tileMargin: 10,
+		};
+		
+		function arrangeBlocks(apps, rowCnt){
+			var l = [];
+			var rc = 0;
+			for(var i=0;i<apps.length;i++){
+				if(l[rc] == null) l[rc] = [];
+				l[rc].push(i);
+				rc = (rc + 1) % rowCnt;
+			}
+			return l;
+		}
+		
+		this.arrangeFunc = arrangeBlocks;
+		
+ 		this.reinitialize = function(){
+			this.oWrapper.empty();
+			if(this._apps == null)
+				return;
+			var root = this.oWrapper;
+			var options = this.options;
+			var apps = this._apps;
+			root.css('min-height', options.tileSize + options.tileMargin * 2);
+			var totalH = root.height();
+			var rowCnt = Math.floor((totalH - options.tileMargin) / (options.tileSize+options.tileMargin));
+			
+			//console.log('rowCnt: '+ rowCnt + ' ' + totalH);
+			var arr = this.arrangeFunc(this._apps, rowCnt);
+			//console.log(arr);
+			
+			//render the blocks
+			var maxwc = 0;
+			var m = options.tileMargin;
+			for(var row=0;row<arr.length;row++){
+				if(arr[row].length == 0)
+					continue;
+				var strm = m + 'px 0 0 ' + m + 'px';
+				var cwc = 0;
+				for(var col=0;col<arr[row].length;col++){
+					var idx = arr[row][col];
+					var wc = apps[idx].width;
+					var w  = wc * options.tileSize + (wc-1) * m;
+					var obj = $('<div class="metrosubblock"></div>').appendTo(root);
+					obj.width(w).height(options.tileSize).css('margin', strm).css('background', 'blue');
+					cwc += wc;
+				}
+				if(cwc > maxwc)
+					maxwc = cwc;
+			}
+			root.width( maxwc * (options.tileSize + m) + m);
+		}
+		
+		this.setContent = function(jsonstr){
+			var apps = $.parseJSON(jsonstr);
+			$.each(apps, function(idx){
+				//console.log(apps[idx].name);
+			});
+			
+			this._apps = apps;
+			//this.reinitialize();
+			return this;
+		}
+		
+		this.reinitialize();
+	}
+})( jQuery );
+
+//global
+var scrollObj = null;
+
 $(window).resize(function(){
 	setBlocksHeight();
+	$('.metroblock').jMetroBlock('reinitialize');
+	if(scrollObj)
+		scrollObj.reinitialize(true);
 });
 
+function genTest(){
+	var cnt = 2 + Math.floor(Math.random() * 8);
+	var s = '[';
+	for(var i=0;i<cnt;i++){
+		var t = '{"name":"A2", "width":';
+		var w = 1 + Math.floor(Math.random() * 3);
+		t += w;
+		t += '}'
+		if(i!=0)
+			s += ',';
+		s += t;
+	}
+	s += ']';
+	console.log(s);
+	return s;
+}
+
 $(document).ready(function(){
-	for(var i=0;i<10;i++){
-		$('<div class="block">AAA'+i+'</div>').appendTo('#contentinner');
+	for(var i=0;i<2;i++){
+		var obj = $('<div class="metroblock"></div>').appendTo('#contentinner').jMetroBlock();
+		obj.jMetroBlock('setContent', genTest());
 	}
 	setBlocksHeight();
-	$('.block').width(400);
-	$('#contentinner').width(getBlocksWidth());
+	//$('.metroblock').width(400);
 	
-	var scrollObj = $('#main-scroll').jMainScroll($('#content'), $('#contentinner'));
-	// using the event helper
+	$('.metroblock').jMetroBlock('reinitialize');
+	$('#contentinner').width(getBlocksWidth());
+	scrollObj = $('#main-scroll').jMainScroll($('#content'), $('#contentinner'));
 
 });
